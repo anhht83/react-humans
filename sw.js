5cc0ba51cc305a6d868373ecea17ae666e0dd45e
Tuan Anh <anhht83@gmai.com>
2020-10-16 02:35:52 +0700

    Updates

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@ var __wpo = {
     ],
     "additional": [
       "/npm.intl.d3b309cd1f41ae274e7d.chunk.js",
-      "/main.958dc2ca778cbd361522.chunk.js",
+      "/main.33626185edee5c8d3702.chunk.js",
       "/npm.babel.6abce3f965f8c61bd5d3.chunk.js",
       "/npm.dom-helpers.2e853abb091244673170.chunk.js",
       "/npm.formik.051b58d438b3f8e731bf.chunk.js",
@@ -28,7 +28,7 @@ var __wpo = {
     "5e53ef208f3fa79a48f8374488f473c76e1233de": "/favicon.ico",
     "71351e898e110e2088c3fb08ebcdd19c97dfb372": "/human-icon.png",
     "1e7fd4463861aeb6a378cf7fb56273f34f47d204": "/npm.intl.d3b309cd1f41ae274e7d.chunk.js",
-    "eb631b14b72c1fba3bcd911d021cd69f8377f342": "/main.958dc2ca778cbd361522.chunk.js",
+    "a76080a330ac10501d1ca1596b44d96840ab5d06": "/main.33626185edee5c8d3702.chunk.js",
     "cb8d91f0839aa728b6b311b10efd83ca5029f0b1": "/npm.babel.6abce3f965f8c61bd5d3.chunk.js",
     "a32dab8549cd0e581e3a037fbfc07dbcf391c494": "/npm.dom-helpers.2e853abb091244673170.chunk.js",
     "16839658831cbc01522694fa3ea3ebb599461708": "/npm.formik.051b58d438b3f8e731bf.chunk.js",
@@ -41,11 +41,11 @@ var __wpo = {
     "2b72131365fd614e1dfd9f28d52ad0c80c89e3da": "/runtime.428fd38c895ec3a83565.js",
     "f8d0a68f0a1889f45800584fad8c07e8c2882906": "/12.1517f65ce216f11e9577.chunk.js",
     "740b263fe7cb95461c76501ca6ed181d0dacc214": "/13.d1f9786e0415ee7fe5c0.chunk.js",
-    "76faf33ac867b33918e136ba3b40b58b87692ce9": "/"
+    "54a6c7b19c450245b297917cbbc8cc83db959d4e": "/"
   },
   "strategy": "changed",
   "responseStrategy": "cache-first",
-  "version": "10/16/2020, 2:33:30 AM",
+  "version": "10/16/2020, 2:35:44 AM",
   "name": "webpack-offline",
   "pluginVersion": "5.0.6",
   "relativePaths": false
